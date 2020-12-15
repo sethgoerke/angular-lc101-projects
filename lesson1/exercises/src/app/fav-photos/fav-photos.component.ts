da8c2e079ab481ca8950f426625a98f7f26af909
Seth Goerke <sethgoerke@hotmail.com>
2020-12-15 13:54:27 -0600

    up to 28.7.3 part2

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'WOW! That is a great picture!';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://th.bing.com/th/id/OIP.dHJjD95U-iuE1xgwNx3iBAHaDR?w=301&h=154&c=7&o=5&dpr=1.25&pid=1.7';
+  image3 = 'https://www.bing.com/th?id=OIP.UedqWbNIJjjbY6N0bq7oOAHaEw&w=226&h=160&c=8&rs=1&qlt=90&dpr=1.25&pid=3.1&rm=2';
 
   constructor() { }
 
